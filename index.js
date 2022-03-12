b47412c66600d9f128faf648d93151632741ef76
zyanx <98738565+zyanx@users.noreply.github.com>
2022-03-12 11:56:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Yo boi!!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a http://localhost:${port}`)
-);
 
 const { Client, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
